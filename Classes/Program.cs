8ce4b9a57bcd66964a5dcf7cab2919ef0a20b737
renmey <deadbeat686@gmail.com>
2026-04-01 04:04:26 +0300

    Добавьте файлы проекта.

---
+++ Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Unit unit1 = new Unit();
+            Unit unit2 = new Unit("MyUnit");
+
+            Console.WriteLine("unit 1.\nname: " + unit1.Name + "\ndamage: " + unit1.Damage + "\nhealth: " + unit1.Health);
+            Console.WriteLine();
+            Console.WriteLine("unit 2.\nname: " + unit2.Name + "\ndamage: " + unit2.Damage + "\nhealth: " + unit2.Health);
+
+            Console.WriteLine(unit1.Name+ " is dead? - " + unit1.SetDamage(1000));
+
+            Console.WriteLine();
+
+            Console.WriteLine(unit2.Name + " is dead? - " + unit2.SetDamage(100));
+
+
+
+        }
+    }
+}
---
+++ Classes/Unit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Unit
+    {
+        private float health = 100f; // поле здоровья, значение возвращается через свойство
+
+        public string Name { get ; }
+        public int Damage {  get; }
+        public float Armor { get;}
+
+        public float Health
+        {
+            get { return this.health; }
+        }
+
+
+        public Unit(): this("Unknown unit") { }
+           
+
+        public Unit(string name)
+        {
+            this.Name = name;
+            Damage = 5;
+            Armor = 0.6f;
+            
+        }
+
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage(float value)
+        {
+            health = health - value * Armor;
+
+            if (Health <= 0f)
+            {
+                return true; //юнит погиб
+            }
+            return false; //юнит жив
+        }
+
+        
+
+    }
+}
